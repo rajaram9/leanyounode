05a4f466580e89ebb37fa98146412b2ce1b86efa
shgtkshruch <shgtk.shruch@gmail.com>
2014-07-14 16:34:55 +0900

    Add lesson4, my first async i/o

---
+++ async-io/io.js
@@ -0,0 +1,17 @@
+// my code
+var fs = require('fs');
+fs.readFile(process.argv[2], 'utf-8', function (err, data) {
+  if (err)
+    throw err;
+  console.log(data.split('\n').length - 1);
+});
+
+// Answer
+var fs = require('fs')
+var file = process.argv[2]
+
+fs.readFile(file, function (err, contents) {
+  // fs.readFile(file, 'utf8', callback) can also be used
+  var lines = contents.toString().split('\n').length - 1
+  console.log(lines)
+})
