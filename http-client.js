e162b0aa1e5c59cba88c20f1a48d5f7e6bc95fbf
shgtkshruch <shgtk.shruch@gmail.com>
2014-07-14 18:21:50 +0900

    Add lesson7, http client

---
+++ http-client.js
@@ -0,0 +1,19 @@
+var http = require('http');
+var url = process.argv[2];
+
+http.get(url, function (response) {
+  response.setEncoding('utf-8');
+  response.on('data', function (data) {
+    console.log(data);
+  })
+})
+
+
+// Answer
+// var http = require('http')
+//
+// http.get(process.argv[2], function (response) {
+//   response.setEncoding('utf8')
+//   response.on('data', console.log)
+//   response.on('error', console.error)
+// })
