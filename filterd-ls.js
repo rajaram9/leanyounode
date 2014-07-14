1c973c62eae861c2b1e2eb4673a956f4f98e827a
shgtkshruch <shgtk.shruch@gmail.com>
2014-07-14 17:12:16 +0900

    Add lesson5, filterd ls

---
+++ filterd-ls.js
@@ -0,0 +1,13 @@
+var fs = require('fs');
+var path = require('path');
+var file = process.argv[2];
+var extension = process.argv[3];
+
+fs.readdir(file, function (err, list) {
+  if (err)
+    throw err;
+  list.forEach(function (file) {
+    if (path.extname(file) === '.' + extension)
+      console.log(file);
+  })
+})
