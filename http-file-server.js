aaa10be4b45fb79e4791f8851d3d392a5a84dc56
shgtkshruch <shgtk.shruch@gmail.com>
2014-07-15 12:01:22 +0900

    Add lesson11, http file server

---
+++ http-file-server.js
@@ -0,0 +1,11 @@
+var http = require('http'),
+    fs = require('fs'),
+    port = process.argv[2],
+    path = process.argv[3];
+
+var server = http.createServer(function(req, res) {
+  res.writeHead(200, {'content-type': 'text/plain'})
+  fs.createReadStream(path).pipe(res);
+})
+
+server.listen(port);
